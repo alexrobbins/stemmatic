14db83d1cddcf31b906e03a487d1bd51253d2af1
Alex Robbins <alexander.j.robbins@gmail.com>
2013-04-30 13:54:00 -0500

    Setup polyglot.

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]]
   :plugins [[lein-ring "0.8.3"]]
+  :source-paths ["src/clojure"]
+  :java-source-paths ["src/java"]
   :ring {:handler stemmatic.handler/app}
   :profiles
   {:dev {:dependencies [[ring-mock "0.1.3"]]}})
