297a428f4b57c4ff68b07d7d4423ad571d6d525f
Alex Robbins <alexander.j.robbins@gmail.com>
2013-05-08 16:15:03 -0500

    Add clojurescript.

--- project.clj
+++ project.clj
@@ -5,10 +5,17 @@
                  [org.clojure/data.priority-map "0.0.2"]
                  [compojure "1.1.5"]
                  [hiccup "1.0.3"]]
-  :plugins [[lein-ring "0.8.3"]]
+  :plugins [[lein-cljsbuild "0.3.0"]
+            [lein-ring "0.8.3"]]
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
   :ring {:handler stemmatic.handler/app}
   :profiles
   {:dev {:dependencies [[ring-mock "0.1.3"]]}}
+  :cljsbuild
+  {:builds
+   [{:source-paths ["src/cljs"]
+     :compiler {:output-to "resources/public/js/main.js"
+                :optimizations :whitespace
+                :pretty-print true}}]}
   :main stemmatic.core)
