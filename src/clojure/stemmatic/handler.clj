fc717663f2ec86d31136ea8cb49d62babf05b2a0
Alex Robbins <alexander.j.robbins@gmail.com>
2013-05-07 22:55:28 -0500

    Stub out urls.

--- src/clojure/stemmatic/handler.clj
+++ src/clojure/stemmatic/handler.clj
@@ -3,9 +3,26 @@
   (:require [compojure.handler :as handler]
             [compojure.route :as route]))
 
+(defn show-graph
+  "Show the document stemma."
+  []
+  "Main graph")
+
+(defn show-doc
+  "Show a document's text."
+  [doc-name]
+  doc-name)
+
+(defn show-diff
+  "Show the diff between two documents."
+  [doc-name-1 doc-name-2]
+  (str "Diff of " doc-name-1 " and " doc-name-2))
+
 (defroutes app-routes
-  (GET "/" [] "Hello World")
-  (route/resources "/")
+  (GET "/" [] (show-graph))
+  (GET "/doc/:doc" [doc] (show-doc doc))
+  (GET "/diff/:doc1/:doc2" [doc1 doc2] (show-diff doc1 doc2))
+  (route/resources "/static/")
   (route/not-found "Not Found"))
 
 (def app
--- test/stemmatic/test/handler.clj
+++ test/stemmatic/test/handler.clj
@@ -7,7 +7,17 @@
   (testing "main route"
     (let [response (app (request :get "/"))]
       (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
+      (is (= (:body response) "Main graph"))))
+
+  (testing "show-doc"
+    (let [response (app (request :get "/doc/test-name"))]
+      (is (= (:status response) 200)
+          (= (:body response) "test-name"))))
+
+  (testing "show-diff"
+    (let [response (app (request :get "/diff/name1/name2"))]
+      (is (= (:status response) 200)
+          (= (:body response) "Diff of name1 and name2"))))
 
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
