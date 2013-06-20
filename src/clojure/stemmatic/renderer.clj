93d1df4d555450b19f5776a8d56ec74135e844f4
Alex Robbins <alexander.j.robbins@gmail.com>
2013-06-19 23:19:21 -0500

    Fixes for renderer and tests.

--- src/clojure/stemmatic/renderer.clj
+++ src/clojure/stemmatic/renderer.clj
@@ -5,14 +5,40 @@
 (defn render-graph
   "Render the graph of the document stemma."
   [state]
-  "Main graph")
+  (list
+   (when-not (empty? state)
+     "Main graph")
+   "Upload docs form"))
 
 (defn render-doc
   "Render the document."
   [docs doc-name]
-  doc-name)
+  (if-let [doc (docs doc-name)]
+    (let [aliases (:aliases doc)]
+      (html
+       [:h2 doc-name]
+       (when-not (empty? aliases)
+         [:h3 "Aliases"]
+         [:div (apply str (interpose ", " aliases))])
+       [:pre
+        (docs doc-name)]))
+    nil))
 
 (defn render-diff
   "Render the diff of the two documents."
   [docs doc-name-1 doc-name-2]
   (str "Diff of " doc-name-1 " and " doc-name-2))
+
+(defn list-docs
+  "List all docs currently loaded."
+  [docs]
+  (html [:head [:title "Loaded Documents"]]
+        [:body
+         [:h1 "Loaded Documents"]
+         (for [{doc-name :name} docs]
+           [:div [:a {:href (str "/doc/" doc-name)} doc-name]])]))
+
+(defn add-new-doc
+  "Show add new doc form."
+  []
+  "New doc form")
--- test/stemmatic/test/renderer.clj
+++ test/stemmatic/test/renderer.clj
@@ -2,15 +2,17 @@
   (:require [clojure.test :refer :all]
             [stemmatic.renderer :refer :all]))
 
-(def state {:docs [{:name "a" :content "Doc a"}
-                   {:name "b" :content "Doc b"}]
-            :mst [["a" "b"]]})
+(def state {:docs {"a" {:name "a" :content "Doc a"}
+                   "b" {:name "b" :content "Doc b"}}})
 
 (deftest test-render-graph
-  (is (= (render-graph state) "Main graph")))
+  (is (= (render-graph state) (list "Main graph"
+                                    "Upload docs form"))))
 
 (deftest test-render-doc
-  (is (= (render-doc (:docs state) "test-doc") "test-doc")))
+  (is (= (render-doc (:docs state) "a")
+         "<h2>a</h2><pre content=\"Doc a\" name=\"a\"></pre>")
+      (= (render-doc (:docs state) "not-found") nil)))
 
 (deftest test-render-diff
   (is (= (render-diff (:docs state) "doc1" "doc2") "Diff of doc1 and doc2")))
